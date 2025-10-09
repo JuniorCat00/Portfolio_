7778b0cbeb2de776c0e35158ac966b0692f3d693
Nanao <juniorcatcaster@gmail.com>
2025-10-10 04:22:33 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 window.onload = function() {
   const links = document.querySelectorAll('#btnLink');
   const currentPage = window.location.pathname.split('/').pop();
+//   ลองศึกษาจาก https://www.w3schools.com/js/js_window_location.asp
 
   links.forEach(link => {
     if(link.getAttribute('href') === currentPage) {
       link.classList.add('active');
     }
   });
+
+// ลองศึกษาจาก https://www.youtube.com/watch?v=6dsdZaNbI3Q
 }
+
+window.addEventListener('scroll', () => {
+  const elements = document.querySelectorAll('.reveal');
+  elements.forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      el.classList.add('active');
+    }
+  });
+});
