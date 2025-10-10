d1c7519819eef41ee9eb5e00544548ff6a8843d4
Nanao <juniorcatcaster@gmail.com>
2025-10-10 17:44:53 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
 window.onload = function() {
   const links = document.querySelectorAll('#btnLink');
   const currentPage = window.location.pathname.split('/').pop();
-//   ลองศึกษาจาก https://www.w3schools.com/js/js_window_location.asp
+//   ลองศึกษาจาก https://www.w3schools.com/js/js_window_location.asp และ https://stackoverflow.com/questions/62319578/issue-when-trying-to-split-url-in-js ไว้สำหรับเช็คดูว่า  html ตรงกับอันไหน
 
   links.forEach(link => {
     if(link.getAttribute('href') === currentPage) {
       link.classList.add('active');
     }
   });
-
 // ลองศึกษาจาก https://www.youtube.com/watch?v=6dsdZaNbI3Q
 }
 
-window.addEventListener('scroll', () => {
-  const elements = document.querySelectorAll('.reveal');
-  elements.forEach(el => {
-    const rect = el.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
-      el.classList.add('active');
-    }
-  });
-});
