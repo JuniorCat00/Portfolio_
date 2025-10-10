77c8e6ab1590f671a7b6811c6dbe158912688298
Nanao <juniorcatcaster@gmail.com>
2025-10-10 17:55:40 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 window.onload = function() {
   const links = document.querySelectorAll('#btnLink');
-  const currentPage = window.location.pathname.split('/').pop();
+  let currentPage = window.location.pathname.split('/').pop();
 //   ลองศึกษาจาก https://www.w3schools.com/js/js_window_location.asp และ https://stackoverflow.com/questions/62319578/issue-when-trying-to-split-url-in-js ไว้สำหรับเช็คดูว่า  html ตรงกับอันไหน
+  
+  if(currentPage === ""){
+    currentPage = "index.html"
+  }
 
   links.forEach(link => {
     if(link.getAttribute('href') === currentPage) {
